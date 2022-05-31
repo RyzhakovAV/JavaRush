4456d442301fa5fc146b451f6911138aff209be9
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-31 20:56:54 +0500

    initial value

---
+++ src/Example_003.java
@@ -0,0 +1,9 @@
+//Напиши программу, которая в методе main объявляет такие переменные:
+//        name типа String, age типа int и city типа String.
+public class Example_003 {
+    public static void main (String[] args) {
+        String name;
+        int age;
+        String city;
+    }
+}
