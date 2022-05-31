960db01e701cf8073ccd3ee3d9efe4d99ffb8c98
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-05-31 18:29:56 +0500

    Initial commit

---
+++ src/Example_001.java
@@ -0,0 +1,6 @@
+public class Example_001 {
+//Напиши программу, которая выводит на экран надпись: "Амиго очень умный".
+   public static void main(String[] args) {
+        System.out.print("Амиго очень умный");
+   }
+}
