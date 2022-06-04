02a500b069746c3f411836bb913a1e63fce43410
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-06-04 15:46:15 +0500

    commented in java

---
+++ src/Example_006.java
@@ -0,0 +1,16 @@
+//Убери комментарии с части кода, чтобы на экран вывелось сообщение "Happy New Year".
+
+public class Example_006 {
+    public static void main(String[] args) {
+        // String s = "Merry";
+        // System.out.println("Christmas");
+        // System.out.println("Merry New");
+        // System.out.println("weekend");
+        System.out.print("Happy New");
+        // System.out.println("weekend");
+        // System.out.println(s);
+        // System.out.print("Merry New!");
+        System.out.print(" ");
+        System.out.println("Year");
+    }
+}
