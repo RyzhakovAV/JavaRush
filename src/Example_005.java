8a5e1557a556c29b270e06b88b8416cf5fc5f750
Aleksandr Ryzhakov <rjizhakov@mail.ru>
2022-06-04 15:21:44 +0500

    initial value int and subtract

---
+++ src/Example_005.java
@@ -0,0 +1,10 @@
+/*
+Сейчас 3126 год. Мой друг родился 8 лет назад.
+Написать программу, выводящую на экран год рождения моего друга.
+ */
+public class Example_005 {
+    public static void main (String[] args) {
+        int year_now = 3126;
+        System.out.print(year_now - 8);
+    }
+}
